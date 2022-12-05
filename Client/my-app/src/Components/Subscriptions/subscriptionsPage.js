1680a43c83b5a3877908d3e9aba6eea3170fce8f
IdanYosef1 <93710408+IdanYosef1@users.noreply.github.com>
2022-12-05 23:33:28 +0200

    Update subscriptionsPage.js

--- Client/my-app/src/Components/Subscriptions/subscriptionsPage.js
+++ Client/my-app/src/Components/Subscriptions/subscriptionsPage.js
@@ -1,38 +1,46 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { updateMessage } from '../../Redux/actions';
-import MainPage from '../mainPage';
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { updateMessage } from "../../Redux/actions";
+import Navbar from "../Navbar/navbar";
 
 function SubscriptionsPage() {
   let history = useHistory();
   const dispatch = useDispatch();
   let store = useSelector((state) => state.permissions);
-  
+
   // Go to the Subscriptions.
   // All Members (default).
   const allMembers = () => {
-    history.push('/subscriptions');
-  }
+    history.push("/subscriptions");
+  };
 
   /* Go to the Add member and updating the message in the redux Store
      that indicates whether a username exists in the system */
   const addMember = () => {
-    dispatch(updateMessage(''));
-    history.push('/addMember');
-  }
+    dispatch(updateMessage(""));
+    history.push("/addMember");
+  };
 
-    // Checking permission and if there is no permission then the button is not displayed.
-  const clickAddMember = (store.Create_Subscriptions) ? addMember  : null;
+  // Checking permission and if there is no permission then the button is not displayed.
+  const clickAddMember = store.Create_Subscriptions ? addMember : null;
 
-  let colorAddUser = 'clickAllAdd', colorEditUser = 'clickAllAdd';
-  (window.location.pathname === '/addMember') ? colorAddUser = 'AllAddButtons' : colorEditUser = 'AllAddButtons' ;
+  let colorAddUser = "clickAllAdd",
+    colorEditUser = "clickAllAdd";
+  window.location.pathname === "/addMember"
+    ? (colorAddUser = "AllAddButtons")
+    : (colorEditUser = "AllAddButtons");
 
-  return <div className="text-center"> 
-     <MainPage/> 
-     <h2>Subscriptions</h2>
-     <button className={colorAddUser} onClick={allMembers}>All Members</button>
-     <button className={colorEditUser} onClick={clickAddMember}>Add Member</button>
-  </div>;
+  return (
+    <div className="text-center">
+      <Navbar />
+      <button className={colorAddUser} onClick={allMembers}>
+        All Members
+      </button>
+      <button className={colorEditUser} onClick={clickAddMember}>
+        Add Member
+      </button>
+    </div>
+  );
 }
 
 export default SubscriptionsPage;
