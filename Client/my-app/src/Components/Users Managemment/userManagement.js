93d2ba32e32b7b0055b8458a94b9508ecb142e7c
IdanYosef1 <93710408+IdanYosef1@users.noreply.github.com>
2022-12-05 23:32:42 +0200

    Update userManagement.js

--- Client/my-app/src/Components/Users Managemment/userManagement.js
+++ Client/my-app/src/Components/Users Managemment/userManagement.js
@@ -1,35 +1,44 @@
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { updateMessage } from "../../Redux/actions";
-import MainPage from '../mainPage';
+import Navbar from "../Navbar/navbar";
 
-function UserManagemnt(){
-    let history = useHistory();
-    const dispatch = useDispatch();
+function UserManagemnt() {
+  let history = useHistory();
+  const dispatch = useDispatch();
 
-
-    /* Go to the Add User and updating the message in the redux Store
+  /* Go to the Add User and updating the message in the redux Store
        that indicates whether a username exists in the system. */
-    const addUser = () => {
-        dispatch(updateMessage(''));
-        history.push('/addUser');
-    }
+  const addUser = () => {
+    dispatch(updateMessage(""));
+    history.push("/addUser");
+  };
 
-    // Go to the User Management.
-    // All Users (default).
-    const allUsers = () => {
-        history.push('/userManagement/admin');
-    }
+  // Go to the User Management.
+  // All Users (default).
+  const allUsers = () => {
+    history.push("/userManagement/admin");
+  };
 
-    let colorAddUser = 'clickAllAdd', colorEditUser = 'clickAllAdd';
-    (window.location.pathname === '/addUser') ? colorAddUser = 'AllAddButtons' : colorEditUser = 'AllAddButtons' ;
-    
-    return <div className="text-center">
-        <MainPage />
-        <h2 >Users</h2>
-        <button className={colorAddUser} onClick={allUsers}>All Users</button>
-        <button className={colorEditUser} onClick={addUser}>Add User</button> <br/><br/>
-    </div>;
+  let colorAddUser = "clickAllAdd",
+    colorEditUser = "clickAllAdd";
+  window.location.pathname === "/addUser"
+    ? (colorAddUser = "AllAddButtons")
+    : (colorEditUser = "AllAddButtons");
+
+  return (
+    <div className="text-center">
+      <Navbar />
+      <button className={colorAddUser} onClick={allUsers}>
+        All Users
+      </button>
+      <button className={colorEditUser} onClick={addUser}>
+        Add User
+      </button>{" "}
+      <br />
+      <br />
+    </div>
+  );
 }
 
 export default UserManagemnt;
